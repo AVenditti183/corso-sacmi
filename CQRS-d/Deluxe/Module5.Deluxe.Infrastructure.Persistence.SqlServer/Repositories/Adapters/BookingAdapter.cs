965b73339ecea75b6c91b1b43ce611b5a3c12127
Antonio Venditti <antonio.venditti@blexin.com>
2023-06-05 17:47:55 +0200

    primo giorno

---
+++ CQRS-d/Deluxe/Module5.Deluxe.Infrastructure.Persistence.SqlServer/Repositories/Adapters/BookingAdapter.cs
@@ -0,0 +1,34 @@
+﻿using Module5.Deluxe.CommandStack.Domain.Model;
+using Module5.Deluxe.Infrastructure.Persistence.SqlServer.Data;
+
+namespace Module5.Deluxe.Infrastructure.Persistence.SqlServer.Repositories.Adapters
+{
+    public static class Adapter
+    {
+        public static Booking RequestToBooking(BookingRequest entity)
+        {
+            var booking = new Booking
+            {
+                CourtId = entity.CourtId,
+                Length = entity.Length,
+                Name = entity.Name,
+                StartingAt = entity.Hour,
+                RequestId = entity.Id.ToString(),
+            };
+            return booking;
+        }
+
+        public static Booking ToBooking(this BookingRequest entity)
+        {
+            var booking = new Booking
+            {
+                CourtId = entity.CourtId,
+                Length = entity.Length,
+                Name = entity.Name,
+                StartingAt = entity.Hour,
+                RequestId = entity.Id.ToString(),
+            };
+            return booking;
+        }
+    }
+}
